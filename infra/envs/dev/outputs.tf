813e41482fa6c988ff5dbdad19927a3e497a60a0
hban1116 <harshitbansal1116@gmail.com>
2025-10-04 18:34:32 +0000

    gitignore

--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -13,7 +13,4 @@ output "private_subnet_ids" {
   value       = module.vpc.private_subnet_ids
 }
 
-output "vpc_cidr_block" {
-  description = "The CIDR block of the VPC"
-  value       = module.vpc.vpc_cidr
-}
+
